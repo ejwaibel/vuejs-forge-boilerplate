51238a7c7a24c2e9418c7cc82d55310bea7b58c6
Erik Waibel <ejwaibel@gmail.com>
2022-07-12 21:15:57 -0700

    Add twicpics npm package

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,16 @@ import router from "./router";
 import "@/assets/base.css";
 import "@progress/kendo-theme-default/dist/all.css";
 
+// import TwicPics vue3 plugin
+import TwicPics from "@twicpics/components/vue3";
+import "@twicpics/components/style.css";
+
 const app = createApp(App);
 
 app.use(router);
+// app is the application instance
+app.use(TwicPics, {
+  domain: "https://orange-pelicans.twic.pics",
+});
 
 app.mount("#app");
